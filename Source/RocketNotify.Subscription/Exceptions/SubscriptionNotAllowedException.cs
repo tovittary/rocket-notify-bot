81b9f187c7323c33b2f4f585de5f6cc11ae27f86
d-kvachev <kvachev.dmitry@gmail.com>
2021-02-15 02:47:20 +0300

    Primitive protection against unauthorized access. The secret key is specified in the text of the message and compared with the one in the configuration. If they do not match - subscription is not allowed.

---
+++ Source/RocketNotify.Subscription/Exceptions/SubscriptionNotAllowedException.cs
@@ -0,0 +1,19 @@
+﻿namespace RocketNotify.Subscription.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Represents an error that occurs when the chat is not allowed to subscribe to notifications.
+    /// </summary>
+    public class SubscriptionNotAllowedException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubscriptionNotAllowedException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public SubscriptionNotAllowedException(string message)
+            : base(message)
+        {
+        }
+    }
+}
--- Source/RocketNotify.TelegramBot/Commands/SubscribeCommand.cs
+++ Source/RocketNotify.TelegramBot/Commands/SubscribeCommand.cs
@@ -1,6 +1,10 @@
 ﻿namespace RocketNotify.TelegramBot.Commands
 {
+    using System;
+    using System.Linq;
     using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Configuration;
 
     using RocketNotify.Subscription.Exceptions;
     using RocketNotify.Subscription.Services;
@@ -18,16 +22,28 @@
         private readonly ISubscriptionService _subscriptionService;
 
         /// <summary>
+        /// Application settings.
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="SubscribeCommand"/> class.
         /// </summary>
         /// <param name="subscriptionService">Notifications subscriptions managing service.</param>
-        public SubscribeCommand(ISubscriptionService subscriptionService)
+        /// <param name="configuration">Application settings.</param>
+        public SubscribeCommand(ISubscriptionService subscriptionService, IConfiguration configuration)
         {
             _subscriptionService = subscriptionService;
+            _configuration = configuration;
         }
 
         /// <inheritdoc />
         public CommandName Name => CommandName.Subscribe;
+
+        /// <summary>
+        /// Gets the name of the command, as it appears in the chat.
+        /// </summary>
+        private string CommandNameInChat => $"/{Name.ToString().ToLower()}";
 
         /// <inheritdoc />
         public async Task<CommandResult> ExecuteAsync(Message message)
@@ -36,11 +52,16 @@
 
             try
             {
+                VerifySubscriptionAllowed(message);
                 await _subscriptionService.AddSubscriptionAsync(chatId).ConfigureAwait(false);
             }
             catch (SubscriberAlreadyExistsException)
             {
                 return new CommandResult { Text = "This chat already subscribed." };
+            }
+            catch (SubscriptionNotAllowedException)
+            {
+                return new CommandResult { Text = "No subscription allowed for this chat. Enter your subscription secret after the command name." };
             }
             catch (SubscriberOperationException)
             {
@@ -48,6 +69,28 @@
             }
 
             return new CommandResult { Text = "Successfully subscribed." };
+        }
+
+        /// <summary>
+        /// Checks whether the chat is allowed to subscribe to notifications or not.
+        /// </summary>
+        /// <param name="message">The message instance.</param>
+        public void VerifySubscriptionAllowed(Message message)
+        {
+            var secret = _configuration.GetSection("Subscriptions")?["Secret"] ?? string.Empty;
+            if (string.IsNullOrWhiteSpace(secret))
+                throw new SubscriptionNotAllowedException("Can't find subscription secret in the configuration file");
+
+            var messageWords = message.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var commandAndArguments = messageWords.SkipWhile(word => !word.StartsWith(CommandNameInChat, StringComparison.InvariantCultureIgnoreCase)).ToArray();
+            if (commandAndArguments.Length < 2)
+                throw new SubscriptionNotAllowedException("Can't get subscription secret from the message");
+
+            var messageSecret = commandAndArguments[1];
+            if (messageSecret.Equals(secret))
+                return;
+
+            throw new SubscriptionNotAllowedException("The subscription secret in the message is invalid");
         }
     }
 }
