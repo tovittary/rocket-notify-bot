ab8958d15931e0b8eec7b7ac3034bfca51b36605
d-kvachev <kvachev.dmitry@gmail.com>
2021-06-04 23:11:56 +0300

    Subscriber info is extended with the configuration of notifications about user mentions in chat messages

---
+++ Source/RocketNotify.Subscription/Model/NotificationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace RocketNotify.Subscription.Model
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Configures how the subscriber should be notified of chat messages.
+    /// </summary>
+    public class NotificationConfiguration
+    {
+        /// <summary>
+        /// Gets or sets the settings for notifications about user mentions.
+        /// </summary>
+        public ICollection<MentionConfiguration> Mentions { get; set; }
+    }
+}
